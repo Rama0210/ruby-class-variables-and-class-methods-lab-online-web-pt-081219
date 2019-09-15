4c934ab94db8c5f757fbc65bd164e28ab46d282a
upbeat-torrent-2014 <ramaabrahams@yahoo.com>
2019-09-15 20:29:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,27 @@
 class Song
+  @@count = 0
+  @@artist = []
+  @@genres = []
 
   attr_accessor :name, :artist, :genre
-  @@count = 0
-  @@artists = []
-  @@genres = []
+#
+  def initialize(name, artist, genre)
+    @@count += 1
+    @@artist << artist
+    @name = name
+    @artist = artist
+    @genre = genre
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.artist
+    @@artist
+  end
+
+  def self.genres
+    @@genres
+  end
+end
